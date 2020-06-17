9ba90f01f3aab1caa7988074ed003fd524470ac8
Fernando Soares <fernando.soares@luizalabs.com>
2020-06-17 00:02:49 -0300

    :fire: Remove console.log

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -34,8 +34,6 @@ const CartProvider: React.FC = ({ children }) => {
         '@GoMarketplace:products',
       );
 
-      console.log(storagedProducts);
-
       if (storagedProducts) {
         setProducts(JSON.parse(storagedProducts));
       }
